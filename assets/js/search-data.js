8023800b25d990ee8e1b85604c6069b56be4ed1b
AngelicaSofia <48481566+AngelicaSofia@users.noreply.github.com>
2026-01-17 12:43:50 +0000

    Deploying to gh-pages from @ AngelicaSofia/AngelicaSofia.github.io@1bf6afb41a4813830c3985777dc26e519d1ee80a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,7 +59,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2026/2025-12-04-LadyMacbeth/";
+            window.location.href = "/blog/2026/2025-12-04-LadyMacbethMtsensk/";
           
         },
       },{id: "post-talent-accelerator-program",
